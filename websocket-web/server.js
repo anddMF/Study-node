a00f0396a3c45228d30787f0dbc9ac4525e54285
Andrew Moraes <andrewmoraesds@gmail.com>
2022-07-09 13:42:06 -0300

    add basic private messaging

--- websocket-web/server.js
+++ websocket-web/server.js
@@ -14,7 +14,8 @@ const io = new Server(server, {
     //     transports: ['websocket', 'polling'],
     //     credentials: true
     // },
-    allowEIO3: true
+    allowEIO3: true,
+    autoConnect: false
 });
 // const io = require('socket.io')(server);
 
@@ -36,26 +37,64 @@ let messages = [];
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
-    socket.emit('receivedMessage', {user: 'CHAT', message: 'Bem vindo!'})
+    socket.emit('receivedMessage', { user: 'CHAT', message: 'Bem vindo!' })
     socket.emit('previousMessages', messages);
+
+    // const users = [];
+    // for (let [id, socket] of io.of("/").sockets) {
+    //     console.log("loop users: ", {
+    //         userID: id,
+    //         username: socket.username,
+    //     });
+    //     users.push({
+    //         userID: id,
+    //         username: socket.username,
+    //     });
+    // }
+
+    //socket.emit('users', users);
 
     socket.on('disconnect', () => {
         console.log(`User desconectado: ${socket.id}`);
     });
 
     socket.on('user connected', (obj) => {
-        socket.broadcast.emit('receivedMessage',{user: 'CHAT', message: `Usuario conectado: ${obj.name}`});
+        socket.broadcast.emit('receivedMessage', { user: 'CHAT', message: `Usuario conectado: ${obj.name}` });
     })
+    
+    socket.on("private message", ({ content, to }) => {
+        socket.to(to).emit("private message", {
+          content,
+          from: socket.id,
+        });
+      });
 
+    socket.onAny((event, ...args) => {
+        console.log('####\nEVENTO: '+event);
+        console.log('DADOS: '+args);
+    })
+      
     // front ta enviando o evento de nome 'chat message'
     socket.on('chat message', (msg) => {
         messages.push(msg);
         console.log('Message: ', msg)
-        
+
         // socket.emit (mensagem exclusivamente para esse socket.id); .on (ouvir uma mensagem); .broadcast.emit (envia para todos os sockets conectados na aplicacao)
         socket.broadcast.emit('receivedMessage', msg)
     })
 })
+
+// io.use((socket, next) => {
+//     const username = socket.handshake.auth.username;
+//     console.log('IO USE: ' + username);
+
+//     if (!username) {
+//         return next(new Error("invalid username"));
+//     }
+
+//     socket.username = username;
+//     next();
+// })
 
 server.listen(3000, () => {
     console.log('listening on 3000')
